f74347f0e5024039b6eb5524636fb0929f5aec8d
anjezzaa <anjeza.murati@gmail.com>
2026-01-22 11:33:51 +0100

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { SortPipe } from './sort.pipe';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports:[DatePipe, DecimalPipe, TemperaturePipe, SortPipe],
+  imports:[DatePipe, TemperaturePipe, SortPipe],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
@@ -22,6 +22,10 @@ export class AppComponent {
   historicTemperatures = [
     25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
   ];
+
+   constructor() {
+     this.historicTemperatures.sort((a,b) => a > b ? 1 : -1);
+   };
 
   onReset(index: number) {
     this.historicTemperatures[index] = 18;
